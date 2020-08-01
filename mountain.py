3975b6d65aeca5c2028a38e0876f51e1fba0c015
Joe Markiewicz <joseph.markiewicz@gmail.com>
2020-08-01 12:18:37 -0500

    scrapper test

--- mountain.py
+++ mountain.py
@@ -9,7 +9,7 @@ from config import user_key
 
 def mountain ():
     private_key = user_key()
-    route_id = '105729927' #want to randomize this
+    route_id = '108471503' #want to randomize this
     base_url = 'https://www.mountainproject.com/data/get-routes?routeIds='
 
     #Mountain Project REST API get request and response
@@ -19,7 +19,7 @@ def mountain ():
     raw_response = json.loads(my_request.content)
     raw_response.keys()
     mountain_df = pd.json_normalize(raw_response['routes'])
-    print(mountain_df)
+    #print(mountain_df)
     #Specifying the variables from the data frame
     climb_name = mountain_df['name'].iloc[0]
     climb_type = mountain_df['type'].iloc[0]
@@ -32,9 +32,11 @@ def mountain ():
     climb_location = mountain_df['location'].iloc[0]
     climb_state = climb_location[0]
     climb_city = climb_location[1]
-    climb_area_1 = climb_location[2]
-    climb_area_2 = climb_location[3]
-    climb_area_3 = climb_location[4]
+    #climb_area_1 = climb_location[2]
+    #climb_area_2 = climb_location[3]
+    #climb_area_3 = climb_location[4]
 
+    message = 'Random Route of the day goes to ' + climb_name + ' in ' + climb_city + ', ' + climb_state + '. This is a ' + climb_type + ' route with a difficulty rating of ' + climb_rating + '. Check out the link for more details. Climb on!'
+    print(message)
 
 mountain()
